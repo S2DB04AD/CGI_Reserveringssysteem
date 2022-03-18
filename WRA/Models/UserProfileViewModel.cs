adce11aeb57ac6e35dc206d216a9d37bc0341073
Rik van Haaren <486859@student.fontys.nl>
2022-03-18 13:07:16 +0100

    Add userprofileModel

---
+++ WRA/Models/UserProfileViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthExample.Models {
+    public class UserProfileViewModel {
+        public string EmailAddress { get; set; }
+
+        public string Name { get; set; }
+
+        public string ProfileImage { get; set; }
+    }
+}
