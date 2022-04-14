4032d1af0cc3a7aa7eabd0bb0b56c1cefc24ce9d
Brummer98 <437019@student.fontys.nl>
2022-04-14 11:28:58 +0200

    View gefikst

--- WRA/Controllers/HomeController.cs
+++ WRA/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace tempApp.Controllers
         }
         
         public IActionResult Index() {
-            return null;
+            // return null; (?)
+            return View();
         }
 
         public IActionResult Login() {
