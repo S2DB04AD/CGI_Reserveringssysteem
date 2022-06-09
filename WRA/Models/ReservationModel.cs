3ac0b389f4f98e7eb23ffe0bba3d543ca0e1dcc5
Dirk <dvanderenden@gmail.com>
2022-06-09 13:59:41 +0200

    input type time

--- WRA/Models/ReservationModel.cs
+++ WRA/Models/ReservationModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,7 +12,9 @@ namespace WRA.Models
         public DateTime ResDate { get; set; }
         public bool Used { get; set; }
         public int AmountPeople { get; set; }
+        [DataType(DataType.Time)]
         public TimeSpan StartTime { get; set; }
+        [DataType(DataType.Time)]
         public TimeSpan EndTime { get; set; }
         public int WorkplaceId { get; set; }
         public int RoomNr { get; set; }
